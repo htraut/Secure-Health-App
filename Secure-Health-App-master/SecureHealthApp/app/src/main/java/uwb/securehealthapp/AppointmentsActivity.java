bde58e15d3bb656a5d24429a70af0336ed9de121
joeyguinasso <joeyguinasso@yahoo.com>
2015-09-15 16:23:20 -0700

    Revert "Revert "Start""
    This reverts commit 768b193a12ea520299c6ec007d1943f5e1451528.
    
    Revert "Start"
    
    This reverts commit
    4273df1f682b8b8c4b5b80cdc1029d54f1ae4827.
    
    Start
    
    getting stuff

---
+++ Secure-Health-App-master/SecureHealthApp/app/src/main/java/uwb/securehealthapp/AppointmentsActivity.java
@@ -0,0 +1,15 @@
+package uwb.securehealthapp;
+
+import android.app.Activity;
+
+/**
+ * Created by Administrator on 9/13/2015.
+ */
+public class AppointmentsActivity extends Activity {
+
+
+
+
+
+
+}
---
+++ Secure-Health-App-master/SecureHealthApp/app/src/main/java/uwb/securehealthapp/PreferencesActivity.java
@@ -0,0 +1,9 @@
+package uwb.securehealthapp;
+
+import android.app.Activity;
+
+/**
+ * Created by Administrator on 9/13/2015.
+ */
+public class PreferencesActivity extends Activity {
+}
