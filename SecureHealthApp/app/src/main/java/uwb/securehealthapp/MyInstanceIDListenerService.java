32eaddbf15ad7ea44cab7b78b044a89cc430a908
htraut <htraut@users.noreply.github.com>
2016-04-06 19:00:07 -0700

    Delete MyInstanceIDListenerService.java

--- SecureHealthApp/app/src/main/java/uwb/securehealthapp/MyInstanceIDListenerService.java
+++
@@ -1,9 +0,0 @@
-package uwb.securehealthapp;
-
-import com.google.android.gms.iid.InstanceIDListenerService;
-
-/**
- * Created by herbt on 1/19/2016.
- */
-public class MyInstanceIDListenerService extends InstanceIDListenerService{
-}
